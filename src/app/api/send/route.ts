60a99ae3cbaf6e8ec5d93988cfcb5ee58f1e8c73
J2MarinLujan <148227281+J2MarinLujan@users.noreply.github.com>
2024-04-07 16:36:36 -0500

    fix: environment variables

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -8,8 +8,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: NextRequest, res: NextResponse) {
 	try {
 		const data = await resend.emails.send({
-			from: "Acme <onboarding@resend.dev>",
-			to: ["juanjose.marinlujan@gmail.com"],
+			from:
+				process.env.RESEND_EMAIL_FROM || "Acme <onboarding@resend.dev>",
+			to: process.env.RESEND_EMAIL_TO || "juanjose.marinlujan@gmail.com",
 			text: "",
 			subject: "Contacto - Landing Nueve 11",
 			react: EmailTemplate(await req.json()) as React.ReactElement,
